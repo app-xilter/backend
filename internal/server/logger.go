58c1f6237d0e399226f9007a9982249a31c86b15
Sercan Arğa <mail@sercanarga.com>
2024-03-21 20:25:05 +0300

    updated logger ip address to x-forwarded-for

--- internal/server/logger.go
+++ internal/server/logger.go
@@ -20,6 +20,6 @@ func LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		srw := &statusResponseWriter{ResponseWriter: w}
 		next.ServeHTTP(srw, r)
-		fmt.Printf("%s	%s	%s	%d	%s\n", time.Now().Format("2006/01/02 15:04:05"), r.Method, r.URL, srw.status, r.RemoteAddr)
+		fmt.Printf("%s	%s	%s	%d	%s\n", time.Now().Format("2006/01/02 15:04:05"), r.Method, r.URL, srw.status, r.Header.Get("x-forwarded-for"))
 	})
 }
