64d071eeeed5c6ae6f0451ae23db3ff4e25627d2
Sercan Arğa <mail@sercanarga.com>
2024-03-21 05:47:22 +0300

    modified for gcloud

--- internal/routes/health.go
+++ internal/routes/health.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Health(mux *http.ServeMux) {
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 
 		if _, err := w.Write([]byte("ok")); err != nil {
